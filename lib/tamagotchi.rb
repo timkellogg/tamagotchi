f25b0d8d3c41796b7e6b63f53d30e7082b138561
Travis Crampton & Tim Kellogg <student@epicodus.com>
2015-08-11 14:10:36 -0700

    first commit

---
+++ lib/tamagotchi.rb
@@ -0,0 +1,30 @@
+class Tamagotchi
+
+  define_method(:initialize) do |name|
+    @name = name
+    @nourishment = 50
+    @happiness   = 50
+    @restfulness = 50
+  end
+  define_method(:name) do
+    @name
+  end
+
+  define_method(:nourishment) do
+    @nourishment
+  end
+
+  define_method(:restfulness) do
+    @restfulness
+  end
+
+  define_method(:happiness) do
+    @happiness
+  end
+
+  define_method(:its_alive?) do
+    if @nourishment.eql?(0)
+      false
+    end
+  end
+end
---
+++ spec/tamagotchi_spec.rb
@@ -0,0 +1,30 @@
+require('rspec')
+require('tamagotchi')
+
+describe(Tamagotchi) do
+  describe(:initialize) do
+    it("sets the name and life levels of a new Tamagotchi") do
+      my_pet = Tamagotchi.new("Bob")
+      expect(my_pet.name()).to(eq("Bob"))
+      expect(my_pet.nourishment()).to(eq(50))
+      expect(my_pet.restfulness()).to(eq(50))
+      expect(my_pet.happiness()).to(eq(50))
+    end
+  end
+
+  describe('#name') do
+    it('returns the name of the Tamagotchi') do
+      my_pet = Tamagotchi.new("Bob")
+      expect(my_pet.name()).to(eq("Bob"))
+    end
+  end
+  describe("#its_alive?") do
+    it('is alive if nourishment != 0') do
+      my_pet = Tamagotchi.new("Bob")
+      expect(my_pet.its_alive?()).to(eq(true))
+    end
+
+    # it('is dead if nourishment i')
+  end
+
+end
